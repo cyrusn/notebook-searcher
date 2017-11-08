728e31c1bf64c14bf304c76e7148253d8febb13c
cyrusn <cyrusncy@gmail.com>
2017-11-08 23:47:24 +0800

    update natvigation

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,11 @@
-/* global $ hljs window */
+/* global $ hljs window Paginator navLinks pageKind */
 $(document).ready(e => {
   // press h goto home
   listenKeys()
   // add Responsive Image to `img` tag
   $('img').addClass('img-fluid')
   $('table').addClass('table table-hover table-sm')
-  $('thead').addClass('thead-dark')
+  $('thead').addClass('thead-dark text-center')
   // Enable HighlightJS
   $('pre code').each(function (i, block) {
     hljs.highlightBlock(block)
@@ -44,41 +44,60 @@ function natvigateList (keyCode, event) {
   const onFocus = $('#search-box').is(':focus')
   const searchBox = $('#search-box')
   const isDisabled = searchBox.prop('disabled')
+  const {hasNext, hasPrev, nextPage, prevPage} = Paginator
 
   switch (true) {
-    /*
-      case (keyCode === 221 && !onFocus): // ]
-      // TODO: Next Category
-        break
-      case (keyCode === 219 && !onFocus): // [
-      // TODO: Previous Category
-        break
-      case (keyCode === 78 && !onFocus): // n
-      // TODO: Next Page
-        break
-      case (keyCode === 80 && !onFocus): // p
-      // TODO: Previous Page
-        break
-    */
+    case (keyCode === 221 && !onFocus): // ]
+      navLinks.forEach((link, key, array) => {
+        if (link === window.location.pathname) {
+          const index = (key + 1) % navLinks.length
+          window.location.pathname = array[index]
+        }
+      })
+      break
+    case (keyCode === 219 && !onFocus): // [
+      navLinks.forEach((link, key, array) => {
+        if (link === window.location.pathname) {
+          const index = (key + navLinks.length - 1) % navLinks.length
+          window.location.pathname = array[index]
+        }
+      })
+      break
+    case (keyCode === 78 && hasNext): // n
+      window.location.pathname = nextPage
+      break
+    case (keyCode === 80 && hasPrev): // p
+      window.location.pathname = prevPage
+      break
     case (keyCode === 81 && !onFocus): // q
       window.location.href = '/'
       break
     case (keyCode === 74): // j
-      toggleActive($active)
-      const $next = $active.next()
-      toggleActive($next)
+      if (pageKind === 'page') {
+        let y = $(window).scrollTop()
+        $(window).scrollTop(y + 200)
+      } else {
+        toggleActive($active)
+        const $next = $active.next()
+        toggleActive($next)
 
-      if ($next.length === 0) {
-        toggleActive($list.eq(0))
+        if ($next.length === 0) {
+          toggleActive($list.eq(0))
+        }
       }
       break
     case (keyCode === 75): // k
-      toggleActive($active)
-      const $prev = $active.prev()
-      toggleActive($prev)
+      if (pageKind === 'page') {
+        let y = $(window).scrollTop()
+        $(window).scrollTop(y - 200)
+      } else {
+        toggleActive($active)
+        const $prev = $active.prev()
+        toggleActive($prev)
 
-      if ($prev.length === 0) {
-        toggleActive($list.eq(-1))
+        if ($prev.length === 0) {
+          toggleActive($list.eq(-1))
+        }
       }
       break
     case (keyCode === 191 && !onFocus): // "/"
@@ -111,8 +130,8 @@ function toggleActive (el) {
     from: '.text-muted, .text-light',
     to: 'text-muted text-light'
   }, {
-    from: 'text-muted text-light',
-    to: '.text-muted, .text-light'
+    from: '.badge-secondary, .badge-light',
+    to: 'badge-secondary badge-light'
   }]
 
   arr.forEach(({from, to}) => {
